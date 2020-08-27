617c1d45e0cb3bd098b2951a5666a66183a56fa0
Mohammadreza Mahdizadeh <37508607+Metti1371@users.noreply.github.com>
2020-08-27 14:49:46 +0200

    Add files via upload

---
+++ resources/js/main.js
@@ -0,0 +1,42 @@
+$( document ).ready(function() {
+	 $("#get-started").click(function(){
+	  $("#second").show();
+	  $("#first").hide();
+	});
+	
+	 $("#get-connected").click(function(){
+	  $("#third").show();
+	  $("#second").hide();
+	});
+
+	$(".icon").click(function(){
+	  $(this).toggleClass("border")
+	});
+	
+	$("#find-match").click(function(){
+	  $("#fourth").show();
+	  $("#third").hide();
+	});
+	
+	$("#great").click(function(){
+	  $("#fifth").show();
+	  $("#fourth").hide();	  
+	});	
+	
+	$(".black").click(function(){
+	  $(".black").hide();	  
+	  $(".pop-up").hide();	  
+	});	
+	
+	$(".pop-up").click(function(){
+	  $(".black").hide();	  
+	  $(".pop-up").hide();	  
+	});		
+
+	$("#alarm-icon").click(function(){
+	  $("#sixth").show();
+	  $("#fifth").hide();
+	  $('.alarm').get(0).play();
+	});	
+	
+});
