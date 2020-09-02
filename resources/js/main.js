3859f6e10a0f45d9c24a9b7e164ceab86d99ab5f
Mohammadreza Mahdizadeh <37508607+Metti1371@users.noreply.github.com>
2020-09-02 16:14:50 +0200

    Add files via upload

--- resources/js/main.js
+++ resources/js/main.js
@@ -1,43 +1,53 @@
-$( document ).ready(function() {
-	 $("#get-started").click(function(){
-	  $("#second").show();
-	  $("#first").hide();
-	});
-	
-	 $("#get-connected").click(function(){
-	  $("#third").show();
-	  $("#second").hide();
-	});
+$(document).ready(function () {
+    $("#get-started").click(function () {
+        $("#second").show();
+        $("#first").hide();
+    });
 
-	$(".icon-container").click(function(){
-	  $(this).toggleClass("border")
-	});
-	
-	$("#find-match").click(function(){
-	  $("#fourth").show();
-	  $("#third").hide();
-	});
-	
-	$("#great").click(function(){
-	  $("#fifth").show();
-	  $("#fourth").hide();	  
-	});	
-	
-	$(".black").click(function(){
-	  $(".black").hide();	  
-	  $(".pop-up").hide();	  
-	});	
-	
-	$(".pop-up").click(function(){
-	  $(".black").hide();	  
-	  $(".pop-up").hide();	  
-	});		
+    $("#get-connected").click(function () {
+        $("#third").show();
+        $("#second").hide();
+    });
 
-	$("#alarm-icon").click(function(){
-	  $("#sixth").show();
-	  $("#fifth").hide();
-	  $('.alarm').get(0).play();
-	  setTimeout(function(){ $('.alarm').get(0).pause(); }, 2000);	  	  	 
-	});	
+    $(".icon-container").click(function () {
+        $(this).toggleClass("border");
+    });
+
+    $("#find-match").click(function () {
+        $("#fourth").show();
+        $("#third").hide();
+    });
+
+    $(".user").click(function () {
+        $(".black").show();
+		$(this).find('.description').show();
 	
+    });
+
+    $("#great").click(function () {
+        $("#fifth").show();
+        $("#fourth").hide();
+        $(".black").show();
+        $("#alarm-popup").show();
+    });
+
+    $(".black").click(function () {
+        $(".black").hide();
+        $("#alarm-popup").hide();
+        $(".description").hide();
+    });
+
+    $(".pop-up").click(function () {
+        $(".black").hide();
+        $("#alarm-popup").hide();
+    });
+
+    $("#alarm-icon").click(function () {
+        $("#sixth").show();
+        $("#fifth").hide();
+        $(".alarm").get(0).play();
+        setTimeout(function () {
+            $(".alarm").get(0).pause();
+        }, 2000);
+    });
 });
